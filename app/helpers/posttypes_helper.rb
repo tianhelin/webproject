a1bab4d8c76a6455608a574acd433c0d156b74de
Tianhe <tianhelin@hotmail.com>
2016-11-01 11:39:01 +0000

    add the btn class to index link, creating the controller of posttypes

---
+++ app/helpers/posttypes_helper.rb
@@ -0,0 +1,2 @@
+module PosttypesHelper
+end
---
+++ test/controllers/posttypes_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PosttypesControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
