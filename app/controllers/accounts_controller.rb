58848888d8a88070b5a7d4da50c0cf27e5d3fa41
Tianhe <tianhelin@hotmail.com>
2016-11-21 15:35:03 +0000

    finished the account mng page, adding the function of adjusting the adminkey of every user(only super admin with adminkey of 1)

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -4,4 +4,16 @@ def index
   @accounts = User.all
 end
 
+
+def update
+  @account = User.find(params[:id])
+  @account.adminkey = params[:adminkey]
+  @account.update(account_params)
+  redirect_to accounts_path
+end
+
+private
+  def account_params
+    params.require(:user).permit(:adminkey)
+  end
 end
