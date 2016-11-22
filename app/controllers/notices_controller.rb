d3b1508ac8a7e10996c0ab1c58bd732bf45a71cf
Tianhe <tianhelin@hotmail.com>
2016-11-22 10:30:07 +0000

    remove the admin authenticate of notice function, finish the index controller.

--- app/controllers/notices_controller.rb
+++ app/controllers/notices_controller.rb
@@ -1,8 +1,9 @@
 class NoticesController < ApplicationController
-  before_action :authenticate_user!, :checkadmin
+  before_action :authenticate_user!
   
   def index
-    
+    @notices = Notice.where('recipient_id = ?', current_user.id )
+    @notices = @notices.page(params[:page]).per(10)
   end
   
   def create
