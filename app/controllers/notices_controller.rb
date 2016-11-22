f10fa3043ec302ffdaa8e84f85436be379d2d0d1
Tianhe <tianhelin@hotmail.com>
2016-11-22 12:17:03 +0000

    finish the showing notice function

--- app/controllers/notices_controller.rb
+++ app/controllers/notices_controller.rb
@@ -1,5 +1,6 @@
 class NoticesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_notice, :only => :show
   
   def index
     @notices = Notice.where('recipient_id = ?', current_user.id )
@@ -13,6 +14,16 @@ class NoticesController < ApplicationController
     if current_user.id != Notice.find(params[:id]).recipient_id
       redirect_to notices_path
     end
+    @notice.readed = true
+    @notice.save
   end
   
+private
+  def set_notice
+    @notice = Notice.find(params[:id])
+  end
+  
+  def notice_params
+    params.require(:notice).permit(:readed)
+  end
 end
