8e7b2ff4bff84eb5250c19d398c6d3ace1e96e03
Tianhe <tianhelin@hotmail.com>
2017-01-04 06:26:03 +0000

    add basic view to notice new page, and add a user-list for selecting recipient.(need add selection by classroom)

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -1,9 +1,20 @@
 $(document).on('turbolinks:load', function() {
   $(".selectpicker").selectpicker();
   
-    $('.ckeditor:visible').each(function() {
-    var id = $(this).attr('id');
+  $('.ckeditor:visible').each(function() {
+  var id = $(this).attr('id');
     if (!CKEDITOR.instances[id]) {
        CKEDITOR.replace(id);
-    }});
+  }});
+  
+  $('.list-group-item > label').change(function(){
+    $(this).parent().toggleClass("active");
+  });
+  
+  $('a.panel-title').click(function(){
+    event.preventDefault();
+    $(this).children('.fa').toggleClass('hidden');
+    $(this).parent().next().toggleClass("hidden");
+  });
+  
 });
